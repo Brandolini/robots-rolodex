0d429839992e3b8889833437deddc21ee62670f9
Brandolini <revitoop@gmail.com>
2020-01-21 02:37:32 -0600

    Updates

---
+++ precache-manifest.f36a72811a58512bb8dd62a41f32ad3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd9d766198d3a9b3e1770a55ae993ba",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "cda0414b5da5d55f84b5",
+    "url": "/robots-rolodex/static/css/main.0b2ff9a3.chunk.css"
+  },
+  {
+    "revision": "3eb8a1a52041ba9063b0",
+    "url": "/robots-rolodex/static/js/2.43d17c82.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots-rolodex/static/js/2.43d17c82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cda0414b5da5d55f84b5",
+    "url": "/robots-rolodex/static/js/main.5bb7a55b.chunk.js"
+  },
+  {
+    "revision": "a49d27cd48a646941b56",
+    "url": "/robots-rolodex/static/js/runtime-main.e04b0cdf.js"
+  }
+]);
